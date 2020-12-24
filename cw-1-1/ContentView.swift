e4ce7c32ee887cb19908abf01b623a6953ad068b
M1-50-mariam <76564921+M1-50-mariam@users.noreply.github.com>
2020-12-25 01:58:56 +0300

    Create ContentView.swift

---
+++ cw-1-1/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  cw-1-1
+//
+//  Created by Mariam2 on 12/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    ZStack {
+        Image("bg")
+            .resizable()
+            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            .ignoresSafeArea()
+        
+        VStack {
+            Image("top")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Spacer()
+            
+            Text("الحمدلله رب العالمين")
+            
+            Spacer()
+            
+            Image("bottom")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        }
+    }
+    
+   }
+}
+  
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 11")
+    }
+}
